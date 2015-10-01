cd81dcd8dc0240c6ce6c587d81b9c937dc8957b4
Noura <nourelfgr17@gmail.com>
2015-10-01 07:47:05 -0700

    Done

---
+++ Karate/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Karate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+    
+        }
+
+        //to register
+        private void btn1Test_MouseDoubleClick(object sender , MouseButtonEventHandler e)
+        {
+            Home.nWindow = new Home();
+            nWindow.show();
+            this.Close();
+        }
+    }
+
+}
